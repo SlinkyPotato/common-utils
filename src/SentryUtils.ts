e6d9312f7d3f5532812c07101a12180a3dfcdd7c
Brian <patinobrian@gmail.com>
2022-03-13 19:31:58 -0400

    fix: wrap sentry ini

--- src/SentryUtils.ts
+++ src/SentryUtils.ts
@@ -1,21 +1,26 @@
 import * as Sentry from '@sentry/node';
 import { RewriteFrames } from '@sentry/integrations';
 import constants from './constants';
+import LogUtils from './LogUtils';
 
 const SentryUtils = {
   init: (appName: string, appVersion: string) => {
-    Sentry.init({
-      dsn: `${constants.sentryDSN}`,
-      tracesSampleRate: 1.0,
-      release: `${appName}@${appVersion}`,
-      environment: `${process.env.SENTRY_ENVIRONMENT}`,
-      integrations: [
-        new RewriteFrames({
-          root: __dirname,
-        }),
-        new Sentry.Integrations.Http({ tracing: true }),
-      ],
-    });
+    try {
+      Sentry.init({
+        dsn: `${constants.sentryDSN}`,
+        tracesSampleRate: 1.0,
+        release: `${appName}@${appVersion}`,
+        environment: `${process.env.SENTRY_ENVIRONMENT}`,
+        integrations: [
+          new RewriteFrames({
+            root: __dirname,
+          }),
+          new Sentry.Integrations.Http({ tracing: true }),
+        ],
+      });
+    } catch (e) {
+      LogUtils.logError('failed to initialize sentry', e);
+    }
   },
 };
 
