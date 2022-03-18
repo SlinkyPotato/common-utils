6c6e0a1590b915736396ae2e773ee4d8d4e78a74
Brian <patinobrian@gmail.com>
2022-03-18 14:45:37 -0400

    fix: poap code validation against db

--- src/Validate.ts
+++ src/Validate.ts
@@ -1,17 +1,30 @@
 import ValidationError from './errors/ValidationError';
+import {
+  PoapEvent,
+  PoapEventsModel,
+} from '@degen/schema';
 
 const Validate = {
-  poapCode(code?: string): void {
+  async poapCode(code?: string): Promise<void> {
     if (code == null) {
       return;
     }
     const POAP_CODE_REGEX = /^[\w\s\W]{1,50}$/;
     if (!POAP_CODE_REGEX.test(code)) {
       throw new ValidationError(
-        'Please enter a poap code: \n' +
+        'Please enter a POAP code: \n' +
                 '- 50 characters maximum\n ' +
                 '- alphanumeric\n ' +
                 '- special characters: .!@#$%&,?');
+    }
+    
+    const result = await PoapEventsModel.findOne<PoapEvent>({
+      isActive: true,
+      'twitterEventMetadata.poapCode': code,
+    });
+    
+    if (result) {
+      throw new ValidationError('Please try another POAP code.');
     }
   },
 
