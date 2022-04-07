6aabd1de907b99bb2cbec7b54fc0a120f22fc03f
Brian <patinobrian@gmail.com>
2022-04-07 12:58:48 -0400

    feat: add thread to ValidationError

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,34 @@ import SentryUtils, { command, message_event } from './SentryUtils';
 import { LogOptions } from '@logdna/logger';
 import EnvConstants from './EnvConstants';
 import Validate from './Validate';
+import ValidationError from './errors/ValidationError';
 
 export { LogOptions };
 
+// SentryUtils
 export {
+  SentryUtils,
   command,
   message_event,
 };
 
+// LogUtils
 export {
   Log,
   LogUtils,
-  SentryUtils,
-  EnvConstants,
+};
+
+// Validation Utils
+export {
   Validate,
+};
+
+// Errors
+export {
+  ValidationError,
+};
+
+// Constants
+export {
+  EnvConstants,
 };
